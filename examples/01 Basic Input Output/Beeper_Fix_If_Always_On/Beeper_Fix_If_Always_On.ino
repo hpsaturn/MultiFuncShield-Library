2303a1225620c4978f0f352ce91caba685d58e09
John Parts Taylor <parts@LumiGeek.com>
2021-05-30 16:06:51 -0700

    Added example project for usage of MFS.setBeeperReversePolarity() to fix some MFS shields where beeper polarity is reversed

---
+++ examples/01 Basic Input Output/Beeper_Fix_If_Always_On/Beeper_Fix_If_Always_On.ino
@@ -0,0 +1,26 @@
+#include <TimerOne.h>
+#include <Wire.h>
+#include <MultiFuncShield.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Timer1.initialize();
+  
+  MFS.setBeeperReversePolarity(true);  // try setting to true if your beeper is always on by default with KNARCO brand boards
+
+  MFS.initialize(&Timer1);    // initialize multi-function shield library
+  
+  // 4 short beeps, repeated 3 times.
+  MFS.beep(5,    // beep for 50 milliseconds
+           5,    // silent for 50 milliseconds
+           4,    // repeat above cycle 4 times
+           3,    // loop 3 times
+           50    // wait 500 milliseconds between loop
+           );
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
